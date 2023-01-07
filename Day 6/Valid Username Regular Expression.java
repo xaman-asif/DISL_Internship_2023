c58ebbaf185d21fc80b062723dc87d77d1d2352e
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-01-07 22:45:02 +0600

    Solved Valid Username Regular Expression Problem

---
+++ Day 6/Valid Username Regular Expression.java
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/valid-username-checker/problem?isFullScreen=true
+
+import java.util.Scanner;
+
+class UsernameValidator {
+  /*
+   * Write regular expression here.
+   */
+  // public static final String regularExpression = null;
+  public static final String regularExpression = "^[A-Za-z][A-Za-z0-9_]{7,29}$";
+}
+
+public class Solution {
+  private static final Scanner scan = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    int n = Integer.parseInt(scan.nextLine());
+    while (n-- != 0) {
+      String userName = scan.nextLine();
+
+      if (userName.matches(UsernameValidator.regularExpression)) {
+        System.out.println("Valid");
+      } else {
+        System.out.println("Invalid");
+      }
+    }
+  }
+}
