ee27fd54301395c944fd99bc4d30af54be9890d5
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-01-07 22:24:12 +0600

    Pattern Syntax Checker

---
+++ Day 6/Pattern Syntax Checker.java
@@ -0,0 +1,21 @@
+//https://www.hackerrank.com/challenges/pattern-syntax-checker/problem?isFullScreen=true
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int testCases = Integer.parseInt(in.nextLine());
+    while (testCases > 0) {
+      String pattern = in.nextLine();
+      // Write your code
+      try {
+        Pattern.compile(pattern);
+        System.out.println("Valid");
+      } catch (Exception e) {
+        System.out.println("Invalid");
+      }
+    }
+  }
+}
