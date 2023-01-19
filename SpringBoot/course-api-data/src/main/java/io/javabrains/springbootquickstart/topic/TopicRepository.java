25a6c10fb57333e9f91528776b214633eecd7ef4
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-01-19 17:57:14 +0600

    Implemented SpringBoot CRUD Application

---
+++ SpringBoot/course-api-data/src/main/java/io/javabrains/springbootquickstart/topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package io.javabrains.springbootquickstart.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository {
+	
+
+}
---
+++ SpringBoot/course-api/src/main/java/io/javabrains/springbootquickstart/JavaBrainsCourseApiApplication.java
@@ -0,0 +1,12 @@
+package io.javabrains.springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBrainsCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBrainsCourseApiApplication.class, args);
+	}
+}
