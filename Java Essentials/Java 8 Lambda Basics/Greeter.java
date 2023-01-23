07303f2f40e35834ea168e8c313d61d4f2e7958a
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-01-23 10:50:09 +0600

    Lambdas vs Interface Implementations

---
+++ Java Essentials/Java 8 Lambda Basics/Greeter.java
@@ -0,0 +1,28 @@
+public class Greeter {
+  public void greet(Greeting greeting) {
+    greeting.perform();
+  }
+
+  public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+
+    Greeting lambdaGreeting = () -> System.out.println("Hello World!");
+    Greeting innerClassGreeting = new Greeting() {
+      @Override
+      public void perform() {
+        System.out.println("Hello World!");
+      }
+    };
+
+    greeter.greet(lambdaGreeting);
+    greeter.greet(innerClassGreeting);
+  }
+}
+
+interface MyLambda {
+  void foo();
+}
+
+interface MyAdd {
+  int foo(int a, int b);
+}
