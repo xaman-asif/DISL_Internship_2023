b250802d7f8f91efb6f0a1365ca9f13a976d7d55
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-01-28 20:50:15 +0600

    Understanding the Servlet

---
+++ Java Essentials/JSPs and Servlets/SimpleServletProject/src/main/java/org/koushik/javabrains/SimpleServlet.java
@@ -0,0 +1,22 @@
+package org.koushik.javabrains;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(description = "A simple servlet", urlPatterns = { "/SimpleServletPath" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello from GET method.");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h3>Hello in HTML</h3>");
+	}
+
+}
