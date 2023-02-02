abb4c696dc0c597e4e0d4229f2bf85b159e17958
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-02-02 10:24:11 +0600

    Fixed 'id' generation of Course table

--- Spring Data JPA/spring-data-jpa-course/src/main/java/com/example/demo/course/Course.java
+++ Spring Data JPA/spring-data-jpa-course/src/main/java/com/example/demo/course/Course.java
@@ -12,13 +12,13 @@ public class Course {
 
     @Id
     @SequenceGenerator(
-            name = "student_sequence",
-            sequenceName = "student_sequence",
+            name = "course_sequence",
+            sequenceName = "course_sequence",
             allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "student_sequence"
+            generator = "course_sequence"
     )
     @Column(
             name = "id",
