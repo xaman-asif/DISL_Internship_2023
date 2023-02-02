88c4f07678c816d77605208ad8b106d8ba01cb88
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-02-02 11:12:30 +0600

    Initialized project spring-data-jpa-custom-repository

---
+++ Spring Data JPA/spring-data-jpa-custom-repo/src/main/java/com/example/jpa/springdatajpacustomrepo/SpringDataJpaCustomRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa.springdatajpacustomrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaCustomRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaCustomRepoApplication.class, args);
+	}
+
+}
---
+++ Spring Data JPA/spring-data-jpa-custom-repo/src/test/java/com/example/jpa/springdatajpacustomrepo/SpringDataJpaCustomRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.springdatajpacustomrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaCustomRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
